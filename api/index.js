b1c9a340f0783ede254a01e56bc7352d181dbe57
naomiweb <laurenn.jean@outlook.fr>
2025-05-03 11:48:40 +0200

    Modified the main app port and fixed the login bug

--- api/index.js
+++ api/index.js
@@ -12,7 +12,7 @@ const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:3000"],
     credentials: true,
   })
 );
